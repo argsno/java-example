a1f4fb811d5c77aaa80ceb0080a3eefd9874aae5
黄志强 <huangzhiqiang02@meituan.com>
2019-05-07 11:29:20 +0800

    init

---
+++ src/main/java/com/argsno/App.java
@@ -0,0 +1,13 @@
+package com.argsno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
